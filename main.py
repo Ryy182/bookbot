0f4a800c0c9bf5a79fbd8dbcfa61ddc832e2063f
ryy182 <ryy1864@gmail.com>
2025-10-13 17:46:07 +0100

    added sys.arv for book location

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from stats import get_num_words, get_chars_dict, chars_to_sorted_list
+import sys
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
@@ -30,3 +34,4 @@ def print_report(book_path, num_words, sorted_ch):
 
 
 main()
+
